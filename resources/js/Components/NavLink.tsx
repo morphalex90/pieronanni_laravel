64572a9c5c06ef6af00a6ff105a080a56f84bf0a
morphalex90 <piero.nanni@gmail.com>
2024-08-12 23:41:01 +0100

    correct active class

--- resources/js/Components/NavLink.tsx
+++ resources/js/Components/NavLink.tsx
@@ -2,6 +2,6 @@ import { Link, InertiaLinkProps } from '@inertiajs/react';
 
 export default function NavLink({ active = false, children, ...props }: InertiaLinkProps & { active: boolean }) {
     return (
-        <Link {...props} className={active ? 'active' : ''}>{children}</Link>
+        <Link {...props} className={active ? 'is-active' : ''}>{children}</Link>
     );
 }
