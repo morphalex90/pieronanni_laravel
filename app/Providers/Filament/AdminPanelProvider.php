9652d8a0e8c811ccacd059a90ff95550d22a9245
morphalex90 <piero.nanni@gmail.com>
2024-08-13 15:01:57 +0100

    make it a spa, temporary allow to register

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->spa()
+            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
