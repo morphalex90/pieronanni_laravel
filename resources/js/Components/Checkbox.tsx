8e4aaa938a882f9df08abe99c920271a8b3eafdf
morphalex90 <piero.nanni@gmail.com>
2024-08-13 11:58:48 +0100

    add missing components

---
+++ resources/js/Components/Checkbox.tsx
@@ -0,0 +1,7 @@
+import { InputHTMLAttributes } from 'react';
+
+export default function Checkbox({ className = '', ...props }: InputHTMLAttributes<HTMLInputElement>) {
+    return (
+        <input {...props} type="checkbox" className={className} />
+    );
+}
