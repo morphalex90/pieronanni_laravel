4d77bfcdce036bd5b401c0b83aab0945b31f5364
morphalex90 <piero.nanni@gmail.com>
2024-08-14 13:59:07 +0100

    temp way to show icons

---
+++ resources/views/pdf/icons.blade.php
@@ -0,0 +1,27 @@
+@if ($technology === 'drupal')
+    <img src="{{ url('') }}/svg/drupal.svg" alt="Drupal">
+@endif
+
+@if ($technology === 'html')
+    <img src="{{ url('') }}/svg/html.svg" alt="HTML">
+@endif
+
+@if ($technology === 'laravel')
+    <img src="{{ url('') }}/svg/laravel.svg" alt="Laravel">
+@endif
+
+@if ($technology === 'magento')
+    <img src="{{ url('') }}/svg/magento.svg" alt="Magento">
+@endif
+
+@if ($technology === 'nextjs')
+    <img src="{{ url('') }}/svg/nextjs.svg" alt="Next.js">
+@endif
+
+@if ($technology === 'react')
+    <img src="{{ url('') }}/svg/react.svg" alt="React">
+@endif
+
+@if ($technology === 'wp')
+    <img src="{{ url('') }}/svg/wp.svg" alt="WordPress">
+@endif
