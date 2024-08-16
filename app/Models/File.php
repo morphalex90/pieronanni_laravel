a9fb4f11878ccbc8497dd8f7c948be61ee03b768
morphalex90 <piero.nanni@gmail.com>
2024-08-16 19:54:07 +0100

    cache images

--- app/Models/File.php
+++ app/Models/File.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class File extends Model
@@ -44,7 +45,10 @@ class File extends Model
     public function getUrlAttribute()
     {
         if ($this->uri != null) {
-            return Storage::disk('backblaze')->temporaryUrl($this->uri, now()->addHours(1));
+
+            return Cache::tags('images')->remember('img:' . $this->uri, 3600, function () { // generate 1 hour image link and cache it for 1 hour
+                return Storage::disk('backblaze')->temporaryUrl($this->uri, now()->addHours(1));
+            });
         }
 
         return $this->uri;
