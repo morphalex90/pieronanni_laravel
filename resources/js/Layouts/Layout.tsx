020112106588f188b9544e0787323c2183a6e65c
morphalex90 <piero.nanni@gmail.com>
2024-08-19 14:00:19 +0100

    convert to typescript, remove dynamic padding

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -1,25 +1,21 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect } from "react";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 import Synt from "@/Components/Synt";
 
-
-export default function Layout({ className = '', children }) {
-    const [mainPadding, setMainPadding] = useState(null);
+export default function Layout({ className = '', children }: { className: string, children: ReactNode }) {
 
     useEffect(() => {
         const body = document.body;
         body.removeAttribute('class');
         body.classList.add(className);
-
-        setMainPadding(document.getElementsByClassName('header')[0].offsetHeight);
-    }, [mainPadding, className]);
+    }, [className]);
 
     return (
         <>
             <Synt />
             <Header />
-            <div className="content-wrapper" style={{ paddingTop: (mainPadding !== null ? mainPadding : 61) }}>
+            <div className="content-wrapper">
                 <main id="main-content">
                     {children}
                 </main>
