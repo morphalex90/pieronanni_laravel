c425a8d841c9bb22df4deb682b0e73df41be89ff
morphalex90 <piero.nanni@gmail.com>
2024-08-19 13:49:07 +0100

    convert to tsx, simplify logic

---
+++ resources/js/Components/Header.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import MainMenu from '@/Components/MainMenu';
+
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <header className="header">
+            <div className="header__container">
+
+                <div className="header__menu">
+                    <MainMenu position="header" className={isOpen ? '--open' : ''} />
+                </div>
+
+                <div className="header__burger">
+                    <button type="button" className="header__burger__icon" onClick={() => setIsOpen(!isOpen)} aria-label="Open Menu Mobile" aria-expanded={isOpen ? 'true' : 'false'} aria-controls="main-menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
+
+            </div>
+        </header>
+    );
+}
