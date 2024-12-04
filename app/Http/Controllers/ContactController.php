5870ad5f81b1ff02f8f42238092df6c4e8ee4f1c
morphalex90 <piero.nanni@gmail.com>
2024-12-04 22:03:56 +0000

    remove old code

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -11,14 +11,6 @@ use Illuminate\Support\Str;
 class ContactController extends Controller
 {
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(ContactStoreRequest $request)
