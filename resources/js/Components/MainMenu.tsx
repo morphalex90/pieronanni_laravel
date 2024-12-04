502f0f4150d475b936081b740c27baccb974f913
morphalex90 <piero.nanni@gmail.com>
2024-12-04 20:41:59 +0000

    convert links to use route

--- resources/js/Components/MainMenu.tsx
+++ resources/js/Components/MainMenu.tsx
@@ -9,11 +9,11 @@ export default function MainMenu({ className, position = '' }: { className?: str
                 {position === 'header' &&
                     <li className="has-icon"><a href="https://github.com/morphalex90" title="GitHub" target="_blank" rel="noopener noreferrer"><img src={github} alt="GitHub" title="GitHub" height={26} width={26} /></a></li>
                 }
-                <li><NavLink href="/" active={route().current('homepage')} title="Home">Home</NavLink></li>
-                <li><NavLink href="/about" active={route().current('about')} title="About">About</NavLink></li>
-                <li><NavLink href="/projects" active={route().current('projects')} title="Projects">Projects</NavLink></li>
-                <li><NavLink href="/contact" active={route().current('contact')} title="Contact">Contact</NavLink></li>
-                <li><a href="/cv" title="cv">CV</a></li>
+                <li><NavLink href={route('homepage')} active={route().current('homepage')} title="Home">Home</NavLink></li>
+                <li><NavLink href={route('about')} active={route().current('about')} title="About">About</NavLink></li>
+                <li><NavLink href={route('projects')} active={route().current('projects')} title="Projects">Projects</NavLink></li>
+                <li><NavLink href={route('contact')} active={route().current('contact')} title="Contact">Contact</NavLink></li>
+                <li><a href={route('cv')} title="cv">CV</a></li>
 
                 {position === 'header' &&
                     <li className="has-icon"><a href="https://www.linkedin.com/in/piero-nanni-87407193" title="LinkedIn" target="_blank" rel="noopener noreferrer">
