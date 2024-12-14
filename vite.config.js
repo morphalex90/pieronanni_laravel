6c3d31906939c5cb7183a75e82b82560e68e169d
morphalex90 <piero.nanni@gmail.com>
2024-12-14 18:25:01 +0000

    update lint

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
     plugins: [
-        eslint({
-            exclude: ['**/node_modules/**', '**/vendor/**']
-        }),
         laravel({
             input: 'resources/js/app.tsx',
             ssr: 'resources/js/ssr.tsx',
