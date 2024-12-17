2ec69db0d381a5f42f56c5e29a72cf535d4bad16
morphalex90 <piero.nanni@gmail.com>
2024-12-17 19:08:04 +0000

    better error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,16 +24,33 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if (! app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-                return Inertia::render('Error', ['status' => $response->getStatusCode()])
-                    ->toResponse($request)
-                    ->setStatusCode($response->getStatusCode());
+            if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+                if (!$request->hasSession()) {
+                    $startSession = app(\Illuminate\Session\Middleware\StartSession::class);
+                    $errorsFromSession = app(\Illuminate\View\Middleware\ShareErrorsFromSession::class);
+                    $encryptCookies = app(\Illuminate\Cookie\Middleware\EncryptCookies::class);
+                    $addQueuedCookiesToResponse = app(\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class);
+                    $handleInertiaRequests = app(\App\Http\Middleware\HandleInertiaRequests::class);
+                    $addQueuedCookiesToResponse = app(\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class);
+                    return $encryptCookies->handle($request, function ($request) use ($addQueuedCookiesToResponse, $startSession, $errorsFromSession, $handleInertiaRequests, $response) {
+                        return $addQueuedCookiesToResponse->handle($request, function ($request) use ($startSession, $errorsFromSession, $handleInertiaRequests, $response) {
+                            return $startSession->handle($request, function ($request) use ($errorsFromSession, $handleInertiaRequests, $response) {
+                                return $errorsFromSession->handle($request, function ($request) use ($handleInertiaRequests, $response) {
+                                    return $handleInertiaRequests->handle($request, function ($request) use ($response) {
+                                        return Inertia::render('Error', ['status' => $response->getStatusCode()])->toResponse($request)->setStatusCode($response->getStatusCode());
+                                    });
+                                });
+                            });
+                        });
+                    });
+                } else {
+                    return Inertia::render('Error', ['status' => $response->getStatusCode()])->toResponse($request)->setStatusCode($response->getStatusCode());
+                }
             } elseif ($response->getStatusCode() === 419) {
                 return back()->with([
                     'message' => 'The page expired, please try again.',
                 ]);
             }
-
             return $response;
         });
 
