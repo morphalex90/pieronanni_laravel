c3a6ffe2f5f95dcea09d483cc3b2eb7bf3df4d9a
morphalex90 <piero.nanni@gmail.com>
2025-02-26 22:02:39 +0000

    add missing country relationship

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
@@ -24,4 +25,9 @@ class Contact extends Model
         'ip_address',
         'user_agent',
     ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
