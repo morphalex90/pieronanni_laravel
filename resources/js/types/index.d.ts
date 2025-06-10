a3846d784154b5fbcbb7c60afb7d8a7f7c38939d
morphalex90 <piero.nanni@gmail.com>
2025-06-10 11:57:13 +0100

    updates, remove lucide-react

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,4 +1,3 @@
-import { LucideIcon } from 'lucide-react'
 import type { Config } from 'ziggy-js'
 
 export interface Auth {
@@ -8,18 +7,6 @@ export interface Auth {
 export interface BreadcrumbItem {
     title: string
     href: string
-}
-
-export interface NavGroup {
-    title: string
-    items: NavItem[]
-}
-
-export interface NavItem {
-    title: string
-    href: string
-    icon?: LucideIcon | null
-    isActive?: boolean
 }
 
 export interface SharedData {
