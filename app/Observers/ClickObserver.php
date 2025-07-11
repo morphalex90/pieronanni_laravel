e2fd451c0f9658c011485f3116eb353acbfd3f44
morphalex90 <piero.nanni@gmail.com>
2025-07-11 12:41:22 +0100

    sent device info to telegram

--- app/Observers/ClickObserver.php
+++ app/Observers/ClickObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Click;
 use App\Models\Country;
+use DeviceDetector\DeviceDetector;
 use Exception;
 use Illuminate\Support\Facades\Http;
 
@@ -21,13 +22,28 @@ class ClickObserver
 
     public function created(Click $click): void
     {
-        $message = "PieroNanni - CV\n";
+        $info = [];
 
-        if ($click->country != null) {
-            $message .= '- ' . $click->country->name . "\n";
+        $dd = new DeviceDetector($click->user_agent);
+        $dd->parse();
+
+        if ($dd->isBot()) {
+            $info['botInfo'] = $dd->getBot();
+        } else {
+            $info['clientInfo'] = $dd->getClient();
+            $info['osInfo'] = $dd->getOs();
+            $info['device'] = $dd->getDeviceName();
+            $info['brand'] = $dd->getBrandName();
+            $info['model'] = $dd->getModel();
         }
 
-        $message .= '- ' . $click->user_agent;
+        $message = "PieroNanni - CV\n\n";
+
+        if ($click->country != null) {
+            $message .= '- ' . $click->country->name . "\n\n";
+        }
+
+        $message .= print_r($info,1);
 
         $apiToken = config('services.telegram.key');
 
